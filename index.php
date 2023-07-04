aa308cb2a1d44ace19a18be287186394d5a29442
salicrulucas <saliculucas@gmail.com>
2023-07-04 15:00:48 -0300

    agrego

---
+++ index.php
@@ -0,0 +1,67 @@
+
+Base de datos Sakila
+
+1-Muestra los nombres de los actores en orden alfab�tico por su apellido.
+
+SELECT last_name FROM actor
+
+2-Muestra los nombres de las ciudades en orden alfab�tico.
+
+SELECT city FROM city
+
+3-Muestra los nombres de las categor�as en orden descendente.
+
+SELECT category_id from category 
+ORDER BY category_id DESC
+
+4-Muestra los t�tulos de las pel�culas en orden alfab�tico.
+
+SELECT title FROM film
+
+5- Muestra los nombres de los clientes en orden descendente por su apellido.
+
+SELECT last_name FROM customer 
+ORDER BY last_name DESC
+
+6-Muestra los nombres de los pa�ses en orden alfab�tico.
+
+SELECT country FROM country
+
+7- Muestra los nombres de los idiomas en orden descendente.
+
+SELECT name FROM language 
+ORDER BY name DESC
+
+8- Muestra los nombres de las tiendas en orden alfab�tico.
+
+SELECT store_id FROM store
+
+9- Muestra los nombres de los staffs en orden descendente por su apellido.
+
+SELECT first_name, last_name FROM staff
+ORDER BY last_name DESC
+
+10 - Muestra los nombres de las pel�culas en orden alfab�tico inverso.
+
+SELECT title FROM film
+ORDER BY title DESC
+
+11 - Muestra los nombres de los clientes en orden ascendente por su apellido.
+
+12- Muestra los nombres de las ciudades en orden descendente.
+
+13- Muestra los nombres de las categor�as en orden alfab�tico inverso.
+
+14 - Muestra los nombres de las pel�culas en orden descendente.
+
+15- Muestra los nombres de los clientes en orden alfab�tico por su nombre.
+
+16 - Muestra los nombres de los pa�ses en orden descendente.
+
+17 - Muestra los nombres de los idiomas en orden alfab�tico inverso.
+
+18- Muestra los nombres de las tiendas en orden ascendente.
+
+19- Muestra los nombres de los staffs en orden alfab�tico por su apellido.
+
+20-Muestra los nombres de las pel�culas en orden descendente inverso.
