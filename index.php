9b9cde662dc342f07d71523cd8023626ceff7b3f
salicrulucas <saliculucas@gmail.com>
2023-07-04 15:19:22 -0300

    realice los otros 10

--- index.php
+++ index.php
@@ -48,20 +48,50 @@ ORDER BY title DESC
 
 11 - Muestra los nombres de los clientes en orden ascendente por su apellido.
 
+SELECT first_name, last_name FROM customer 
+ORDER BY last_name
+
 12- Muestra los nombres de las ciudades en orden descendente.
+
+SELECT city FROM city 
+ORDER BY city DESC
 
 13- Muestra los nombres de las categor�as en orden alfab�tico inverso.
 
+SELECT name FROM category
+ORDER BY name DESC
+
 14 - Muestra los nombres de las pel�culas en orden descendente.
+
+SELECT title FROM film
+ORDER BY title DESC
 
 15- Muestra los nombres de los clientes en orden alfab�tico por su nombre.
 
+SELECT first_name FROM customer 
+ORDER BY first_name
+
 16 - Muestra los nombres de los pa�ses en orden descendente.
+
+SELECT country FROM country
+ORDER BY country DESC
 
 17 - Muestra los nombres de los idiomas en orden alfab�tico inverso.
 
+SELECT name	FROM language
+ORDER BY name DESC
+
 18- Muestra los nombres de las tiendas en orden ascendente.
+
+SELECT store_id FROM store
+ORDER BY store_id
 
 19- Muestra los nombres de los staffs en orden alfab�tico por su apellido.
 
+SELECT first_name, last_name FROM staff
+ORDER BY last_name
+
 20-Muestra los nombres de las pel�culas en orden descendente inverso.
+
+SELECT title FROM film
+ORDER BY title DESC
